ac012d37c3a696165989f123a9bfcbbd3d2195e5
ZeyadSabbah <zeyadsabbah2@gmail.com>
2020-12-18 16:36:55 +0000

    add city filter

--- bikeshare.py
+++ bikeshare.py
@@ -16,8 +16,11 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+    # get user input for city (chicago, new york city, washington)
+    cities = ['chicago', 'new york city', 'washington']
+    city = 0
+    while city not in cities:
+        city = input('Which city are you looking to explore? Please, enter one of the 3 cities; Chicago, New York City, or Washington.\n').lower()
 
     # TO DO: get user input for month (all, january, february, ... , june)
 
